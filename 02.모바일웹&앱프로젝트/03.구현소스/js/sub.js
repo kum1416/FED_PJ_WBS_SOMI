4e74d9b418d2a3ffd22ac124c59be947005a6a4a
kum1416 <kum1416@naver.com>
2023-05-15 00:46:29 +0900

    ss

--- 02.모바일웹&앱프로젝트/03.구현소스/js/sub.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/sub.js
@@ -7,22 +7,22 @@ $(() => {
     // 대상선정
     let plist = $(".paper_list");
 
-    let hcode = '';
-
+    let hcode = "";
+    console.log(ename);
     for (let i = 1; i < 24; i++) {
         hcode += `
         <li>
             <a href="#">
                 <div class="p_img">
                     <img
-                        src="./images_EMBOSSED/${i}_LEATHACK.png"
+                        src="./images_EMBOSSED/${i}_${ename}.png"
                         alt="레자크"
                     />
                 </div>
                 <!-- 종이 이름 -->
                 <div class="p_name">
-                    <p>LEATHACK ${i}</p>
-                    <p>레자크 ${i}</p>
+                    <p>${ename[i]}</p>
+                    <p>${ename2[i]}</p>
                 </div>
             </a>
         </li>
@@ -34,32 +34,30 @@ $(() => {
 
     // 2. COLORED PAPER
 
-    
-
     // 2번째 알려주기 둘다 선셋팅을 해놓고 클래스만 주면되게하기
-        // 언제? -> 로딩시
-        // 누구한테? -> .sub_bgt
-        // 뭘해야될까? -> 클래스 on주기 = addClass(클래스명)
-        // console.log('.sub_bgt')
-        // $('.sub_bgt').addClass('on');
+    // 언제? -> 로딩시
+    // 누구한테? -> .sub_bgt
+    // 뭘해야될까? -> 클래스 on주기 = addClass(클래스명)
+    // console.log('.sub_bgt')
+    // $('.sub_bgt').addClass('on');
 
-        a1 = $('.sub_bgt')
-        // 변수2= $('.sub_bwe')
-        // 변수3 = $('.sub_bger')
+    a1 = $(".sub_bgt");
+    // 변수2= $('.sub_bwe')
+    // 변수3 = $('.sub_bger')
 
+    // 함수를 만들어서
+    function adc(x) {
+        //x = 파라미터 ,매개변수 , 전달변수
 
+        // x는 클래스변수 -> x에 클래스on을 넣어주는 함수
+        x.addClass("on");
 
-        // 함수를 만들어서 
-        function adc(x){ //x = 파라미터 ,매개변수 , 전달변수 
+        // 이용방법
+        // 원하는 요소를 변수화 한다음에 asd(변수) 끝
+    }
 
-            // x는 클래스변수 -> x에 클래스on을 넣어주는 함수
-            x.addClass('on')
-
-            // 이용방법
-            // 원하는 요소를 변수화 한다음에 asd(변수) 끝 
-        }
-
-        adc(a1)
-        // adc(a2)
-        // adc(a3)
+    // 호출
+    adc(a1);
+    // adc(a2)
+    // adc(a3)
 });
