adcd051ab555d9d019202b9e29e344a98fa7a6a3
kum1416 <kum1416@naver.com>
2023-05-15 15:49:50 +0900

    SS

--- 02.모바일웹&앱프로젝트/03.구현소스/js/sub.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/sub.js
@@ -15,7 +15,7 @@ $(() => {
             <a href="#">
                 <div class="p_img">
                     <img
-                        src="./images_EMBOSSED/${i}_${ename}.png"
+                        src="./images_EMBOSSED/${i}k.png"
                         alt="레자크"
                     />
                 </div>
@@ -33,6 +33,59 @@ $(() => {
     plist.html(hcode);
 
     // 2. COLORED PAPER
+    // let ccode = "";
+
+    // for (let i = 1; i < 9; i++) {
+    //     ccode += `
+    //     <li>
+    //         <a href="#">
+    //             <div class="p_img">
+    //                 <img
+    //                     src="./images_COLORED/${i}k.png"
+    //                     alt="레자크"
+    //                 />
+    //             </div>
+    //             <!-- 종이 이름 -->
+    //             <div class="p_name">
+    //                 <p>${ename[i]}</p>
+    //                 <p>${ename2[i]}</p>
+    //             </div>
+    //         </a>
+    //     </li>
+    //     `;
+    // }
+
+    // // 출력하기
+    // plist.html(ccode);
+
+    // // 3. PREMIUM PAPER
+    // let pcode = "";
+
+    // for (let i = 1; i < 14; i++) {
+    //     pcode += `
+    //     <li>
+    //         <a href="#">
+    //             <div class="p_img">
+    //                 <img
+    //                     src="./images_PREMIUM/${i}k.png"
+    //                     alt="레자크"
+    //                 />
+    //             </div>
+    //             <!-- 종이 이름 -->
+    //             <div class="p_name">
+    //                 <p>${ename[i]}</p>
+    //                 <p>${ename2[i]}</p>
+    //             </div>
+    //         </a>
+    //     </li>
+    //     `;
+    // }
+
+    // // 출력하기
+    // plist.html(pcode);
+
+
+
 
     // 2번째 알려주기 둘다 선셋팅을 해놓고 클래스만 주면되게하기
     // 언제? -> 로딩시
@@ -61,58 +114,5 @@ $(() => {
     // adc(a2)
     // adc(a3)
 
-    /////// GNB 오버시 서브메뉴 보이기 /////////
-    // 1. 대상선정
-    const mmu = document.querySelectorAll(".gnb li a");
-    console.log(mmu);
-
-    // html 담을 변수
-    let mcode = "";
-
-    // 2. 코드 구조 - 하위 메뉴 반복 코드
-    for(let sub in mdata){
-        mcode +=
-        `
-            <div class="smbx">
-                <ul>
-                    <li>
-                        <a href="#">${sub}</a>
-                    </li>
-                </ul>
-            </div>
-        `
-    }
-    console.log(mdata);
-
-    // 3. GNB 박스에 출력하기
-    mmu.innerHTML = mcode;
- 
- /////// GNB 메뉴 오버시 서브메뉴 보이기 ///////
-
-    // 1. 대상선정
-    // 이벤트 대상: .gnb>ul>li
-    const list = document.querySelectorAll(".gnb>ul>li");
-    console.log(list);
-
-    // 2. 상위메뉴 li에 이벤트 설정하기
-    for(let x of list){
-        if(x.querySelector("a").innerText) {
-            // 마우스 오버시 ///
-            x.onmouseenter = () => {
-                // (1) 하위메뉴 박스 .sub_bx 선택하여 변경하기
-                let tg = x.querySelector(".smbx");
-                // (2) 하위메뉴 style 변경하기
-                tg.style.height = "142px";
-                tg.style.opacity = 1;
-            }; ///// onmouseenter ////////
-
-            // 마우스 오버시 ///
-            x.onmouseleave = () => {
-                let tg = x.querySelector(".smbx");
-                tg.style.height = "0";
-                tg.style.opacity = 0;
-            }; ///// onmouseleave ////////
-        }
-    } //////// for of //////
 
 });
