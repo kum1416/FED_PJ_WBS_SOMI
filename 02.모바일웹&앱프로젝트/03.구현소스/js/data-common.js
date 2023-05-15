a9908fa98845cceb96139bb0fd55265619e57189
kum1416 <kum1416@naver.com>
2023-05-15 17:50:08 +0900

    ss

---
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/data-common.js
@@ -0,0 +1,258 @@
+const subData = {
+  tarea :`
+      <header class="top">
+        <!-- 1.로고파트 -->
+        <div>
+          <h1 class="logo">
+            <a href="#">
+              <!-- 검정로고 -->
+              <img src="./images/logo.png" alt="로고" />
+              <!-- 하얀로고 -->
+              <img src="./images/logo-w.png" alt="로고" />
+            </a>
+          </h1>
+        </div>
+        <!-- 2.네비파트 -->
+        <div>
+          <nav class="gnb">
+            <ul>
+              <!-- 1.ABOUT -->
+              <li>
+                <a href="#">ABOUT</a>
+              </li>
+              <!-- 2.PRODUCTS -->
+              <li>
+                <a href="#">PRODUCTS</a>
+              </li>
+              <!-- 3.SUSTAINABILITY -->
+              <li>
+                <a href="#">SUSTAINABILITY</a>
+              </li>
+              <!-- 4.CONTACT -->
+              <li>
+                <a href="#">CONTACT</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <!-- 3.기타파트 -->
+        <div>
+          <ul class="other">
+            <li>
+              <a href="#">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <!-- <i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i> -->
+              </a>
+            </li>
+            <li>
+              <div>KOR</div>
+            </li>
+            <li>
+              <a href="">
+                <div class="ham">
+                  <i class="fa-solid fa-burger" style="color: #000000"></i>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    
+    `,
+    marea:`
+    <main>
+        <section class="sub_first">
+          <div class="sub_pg1">
+            <div class="sub_bgi1">
+              <img src="./images/sub1_Embossed_img.png" alt="배경이미지">
+            </div>
+            <!-- 배경 텍스트박스 -->
+            <div class="sub_bgt">
+              <h3>EMBOSSED PAPER</h3>
+            </div>
+            <!-- 화살표 -->
+            <div class="arrow">
+              <i class="fa-solid fa-arrow-down-long" style="color: #ffffff"></i>
+            </div>
+          </div>
+          <!-- 종이리스트 -->
+          <div class="sub_paper">
+            <!-- 서브타이틀 -->
+            <div class="sub_tit">EMBOSSED PAPER</div>
+            <ul class="paper_list">
+              <li>
+                <a href="#">
+                  <div class="p_img">
+                    <img src="./images_EMBOSSED/2_LEATHACK.png" alt="레자크" />
+                  </div>
+                  <!-- 종이 이름 -->
+                  <div class="p_name">
+                    <p>LEATHACK 64</p>
+                    <p>레자크 64</p>
+                  </div>
+                </a>
+              </li>
+              <!-- <li>
+                              <a href="#">
+                                  <div class="p_img">
+                                      <img
+                                          src="./images_EMBOSSED/1_LEATHACK 64.png"
+                                          alt="레자크"
+                                      />
+                                  </div>
+      
+                                  <div class="p_name">
+                                      <p>LEATHACK 64</p>
+                                      <p>레작 64</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                                  <div class="p_img">
+                                      <img
+                                          src="./images_EMBOSSED/1_LEATHACK 64.png"
+                                          alt="레자크"
+                                      />
+                                  </div>
+      
+                                  <div class="p_name">
+                                      <p>LEATHACK 64</p>
+                                      <p>이앙 64</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                                  <div class="p_img">
+                                      <img
+                                          src="./images_EMBOSSED/1_LEATHACK 64.png"
+                                          alt="레자크"
+                                      />
+                                  </div>
+      
+                                  <div class="p_name">
+                                      <p>LEATHACK 64</p>
+                                      <p>앙안 64</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                                  <div class="p_img">
+                                      <img
+                                          src="./images_EMBOSSED/1_LEATHACK 64.png"
+                                          alt="레자크"
+                                      />
+                                  </div>
+      
+                                  <div class="p_name">
+                                      <p>LEATHACK 64</p>
+                                      <p>레자크 64</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                                  <div class="p_img">
+                                      <img
+                                          src="./images_EMBOSSED/1_LEATHACK 64.png"
+                                          alt="레자크"
+                                      />
+                                  </div>
+      
+                                  <div class="p_name">
+                                      <p>LEATHACK 64</p>
+                                      <p>레자크 64</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                                  <div class="p_img">
+                                      <img
+                                          src="./images_EMBOSSED/1_LEATHACK 64.png"
+                                          alt="레자크"
+                                      />
+                                  </div>
+      
+                                  <div class="p_name">
+                                      <p>LEATHACK 64</p>
+                                      <p>레자크 64</p>
+                                  </div>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                                  <div class="p_img">
+                                      <img
+                                          src="./images_EMBOSSED/1_LEATHACK 64.png"
+                                          alt="레자크"
+                                      />
+                                  </div>
+      
+                                  <div class="p_name">
+                                      <p>LEATHACK 64</p>
+                                      <p>레자크 64</p>
+                                  </div>
+                              </a>
+                          </li> -->
+            </ul>
+            <!-- 서브-하단메뉴박스 -->
+            <div class="sub_menu">
+              <ul>
+                <!-- 서브-하단메뉴3 -->
+                <li>
+                  <a href="#">
+                    <div class="sub_mtxt sub_bg3">
+                      <div></div>
+                      <h3>PREMIUM PAPER</h3>
+                      <h4>PREV</h4>
+                    </div>
+                  </a>
+                </li>
+                <!-- 서브-하단메뉴2 -->
+                <li>
+                  <a href="#">
+                    <div class="sub_mtxt sub_bg2">
+                      <div></div>
+                      <h3>COLORED PAPER</h3>
+                      <h4>NEXT</h4>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </main>
+    `,
+    barea:`
+          <footer class="info">
+            <!-- 3-1.하단링크 -->
+            <ul class="blink">
+              <li>
+                <a href="#">이용약관</a>
+              </li>
+              <li>
+                <a href="#">개인정보취급방침</a>
+              </li>
+              <li>
+                <a href="#">이메일무단수집거부</a>
+              </li>
+              <li>
+                <a href="#">쿠키정책</a>
+              </li>
+            </ul>
+            <!-- 3-2.회사정보 -->
+            <address class="addr">
+              서울 중구 을지로 170 을지트윈타워 동관 7층 | 사업자등록번호 317-81-26964 | TEL 02) 753-1136 | FAX 02)
+              773-7484<br />
+              ⓒ Samwha Paper Co., Ltd. All rights reserved
+            </address>
+          </footer>
+    `
+}; //
+
+// 내보내기
+export default subData;
--- 02.모바일웹&앱프로젝트/03.구현소스/js/sub.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/sub.js
@@ -1,124 +1,173 @@
 // 서브페이지 자바스크립트
 
-$(() => {
-    // 1. EMBOSSED PAPAR
-    console.log("안녕");
+// 뷰 적용할 서브페이지
 
-    // 대상선정
-    let plist = $(".paper_list");
+// 공통 데이터
+import comData from './data-common.js'
 
-    let hcode = "";
-    console.log(ename);
-    for (let i = 1; i < 24; i++) {
-        hcode += `
-        <li>
-            <a href="#">
-                <div class="p_img">
-                    <img
-                        src="./images_EMBOSSED/${i}k.png"
-                        alt="레자크"
-                    />
-                </div>
-                <!-- 종이 이름 -->
-                <div class="p_name">
-                    <p>${ename[i]}</p>
-                    <p>${ename2[i]}</p>
-                </div>
-            </a>
-        </li>
-        `;
-    }
+// 상단영역 뷰 템플릿 /////
+Vue.component("tarea-comp",{
+    template:comData.tarea,
+}); ///// 상단영역 Vue component /////
 
-    // 출력하기
-    plist.html(hcode);
+// 메인영역 뷰 템플릿 /////
+Vue.component("marea-comp",{
+    template:comData.marea,
+}); ///// 메인영역 Vue component /////
 
-    // 2. COLORED PAPER
-    // let ccode = "";
-
-    // for (let i = 1; i < 9; i++) {
-    //     ccode += `
-    //     <li>
-    //         <a href="#">
-    //             <div class="p_img">
-    //                 <img
-    //                     src="./images_COLORED/${i}k.png"
-    //                     alt="레자크"
-    //                 />
-    //             </div>
-    //             <!-- 종이 이름 -->
-    //             <div class="p_name">
-    //                 <p>${ename[i]}</p>
-    //                 <p>${ename2[i]}</p>
-    //             </div>
-    //         </a>
-    //     </li>
-    //     `;
-    // }
-
-    // // 출력하기
-    // plist.html(ccode);
-
-    // // 3. PREMIUM PAPER
-    // let pcode = "";
-
-    // for (let i = 1; i < 14; i++) {
-    //     pcode += `
-    //     <li>
-    //         <a href="#">
-    //             <div class="p_img">
-    //                 <img
-    //                     src="./images_PREMIUM/${i}k.png"
-    //                     alt="레자크"
-    //                 />
-    //             </div>
-    //             <!-- 종이 이름 -->
-    //             <div class="p_name">
-    //                 <p>${ename[i]}</p>
-    //                 <p>${ename2[i]}</p>
-    //             </div>
-    //         </a>
-    //     </li>
-    //     `;
-    // }
-
-    // // 출력하기
-    // plist.html(pcode);
+// 하단영역 뷰 템플릿 /////
+Vue.component("info-comp",{
+    template:comData.barea,
+}); ///// 하단영역 Vue component /////
 
 
+// 상단영역 뷰 인스턴스 생성하기 ///////
+new Vue({
+    el:'#top',
+    mounted : function() {
+        console.log("안녕");
+        {
+            // 1. EMBOSSED PAPAR
+            console.log("안녕");
+        
+            // 대상선정
+            let plist = $(".paper_list");
+        
+            let hcode = "";
+            console.log(ename);
+            for (let i = 1; i < 24; i++) {
+                hcode += `
+                <li>
+                    <a href="#">
+                        <div class="p_img">
+                            <img
+                                src="./images_EMBOSSED/${i}k.png"
+                                alt="레자크"
+                            />
+                        </div>
+                        <!-- 종이 이름 -->
+                        <div class="p_name">
+                            <p>${ename[i]}</p>
+                            <p>${ename2[i]}</p>
+                        </div>
+                    </a>
+                </li>
+                `;
+            }
+        
+            // 출력하기
+            plist.html(hcode);
+        
+            // 2. COLORED PAPER
+            // let ccode = "";
+        
+            // for (let i = 1; i < 9; i++) {
+            //     ccode += `
+            //     <li>
+            //         <a href="#">
+            //             <div class="p_img">
+            //                 <img
+            //                     src="./images_COLORED/${i}k.png"
+            //                     alt="레자크"
+            //                 />
+            //             </div>
+            //             <!-- 종이 이름 -->
+            //             <div class="p_name">
+            //                 <p>${ename[i]}</p>
+            //                 <p>${ename2[i]}</p>
+            //             </div>
+            //         </a>
+            //     </li>
+            //     `;
+            // }
+        
+            // // 출력하기
+            // plist.html(ccode);
+        
+            // // 3. PREMIUM PAPER
+            // let pcode = "";
+        
+            // for (let i = 1; i < 14; i++) {
+            //     pcode += `
+            //     <li>
+            //         <a href="#">
+            //             <div class="p_img">
+            //                 <img
+            //                     src="./images_PREMIUM/${i}k.png"
+            //                     alt="레자크"
+            //                 />
+            //             </div>
+            //             <!-- 종이 이름 -->
+            //             <div class="p_name">
+            //                 <p>${ename[i]}</p>
+            //                 <p>${ename2[i]}</p>
+            //             </div>
+            //         </a>
+            //     </li>
+            //     `;
+            // }
+        
+            // // 출력하기
+            // plist.html(pcode);
+        
+        
+        
+        
+            // 2번째 알려주기 둘다 선셋팅을 해놓고 클래스만 주면되게하기
+            // 언제? -> 로딩시
+            // 누구한테? -> .sub_bgt
+            // 뭘해야될까? -> 클래스 on주기 = addClass(클래스명)
+            // console.log('.sub_bgt')
+            // $('.sub_bgt').addClass('on');
+        
+            a1 = $(".sub_bgt");
+            // 변수2= $('.sub_bwe')
+            // 변수3 = $('.sub_bger')
+        
+            // 함수를 만들어서
+            function adOn(x) {
+                //x = 파라미터 ,매개변수 , 전달변수
+        
+                // x는 클래스변수 -> x에 클래스on을 넣어주는 함수
+                x.addClass("on");
+        
+                // 이용방법
+                // 원하는 요소를 변수화 한다음에 asd(변수) 끝
+            }
+            // 호출
+            adOn(a1);
+            // adc(a2)
+            // adc(a3)
+        
+            ///// 화살표 클릭시 아래페이지로 이동 /////
+            $(".arrow").click(function(){
+                let winh = $('.sub_bgi1').height()
+                console.log('애로우',winh)
+                $('html,body').animate({
+                    scrollTop:winh
+                },500)
+            })
+        
+            $('.sub_bgi1 img').css({
+                transform: "scale(1.02)",
+                transition: "3s linear"
+            })
+        
+        // 요 배경을 담고있는 박스가 필요
+        //  그 박스에 오버플로우 히든줘서 배경이 나가지않게 
+        
+        
+        
+        }
+    },
+}); //////// 상단영역 뷰 인스턴스 //////
 
+//###### 메인영역 뷰 인스턴스 생성하기 ##########
+new Vue({
+    el:"#middle",
+}) //////// 메인영역 뷰 인스턴스 //////// 
 
-    // 2번째 알려주기 둘다 선셋팅을 해놓고 클래스만 주면되게하기
-    // 언제? -> 로딩시
-    // 누구한테? -> .sub_bgt
-    // 뭘해야될까? -> 클래스 on주기 = addClass(클래스명)
-    // console.log('.sub_bgt')
-    // $('.sub_bgt').addClass('on');
-
-    a1 = $(".sub_bgt");
-    // 변수2= $('.sub_bwe')
-    // 변수3 = $('.sub_bger')
-
-    // 함수를 만들어서
-    function adOn(x) {
-        //x = 파라미터 ,매개변수 , 전달변수
-
-        // x는 클래스변수 -> x에 클래스on을 넣어주는 함수
-        x.addClass("on");
-
-        // 이용방법
-        // 원하는 요소를 변수화 한다음에 asd(변수) 끝
-    }
-    // 호출
-    adOn(a1);
-    // adc(a2)
-    // adc(a3)
-
-    ///// 화살표 클릭시 아래페이지로 이동 /////
-    let arrow = $(".arrow");
-    console.log(arrow);
-
-    
-
-
-
-});
+//###### 하단영역 뷰 인스턴스 생성하기 ##########
+new Vue({
+    el:"#info",
+}) //////// 하단영역 뷰 인스턴스 //////// 
