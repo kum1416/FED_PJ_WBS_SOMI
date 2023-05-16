ee6aa572534f7afe20d4d2700c8f92388ed23f77
kum1416 <kum1416@naver.com>
2023-05-16 15:52:00 +0900

    ss

--- 02.모바일웹&앱프로젝트/03.구현소스/js/data-common.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/data-common.js
@@ -24,7 +24,7 @@ const subData = {
               </li>
               <!-- 2.PRODUCTS -->
               <li>
-                <a href="#">PRODUCTS</a>
+                <a href="./sub.html">PRODUCTS</a>
               </li>
               <!-- 3.SUSTAINABILITY -->
               <li>
