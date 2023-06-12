99e9a91e19eebf8b703897aba72f58aa9c2e952e
kum1416 <kum1416@naver.com>
2023-06-12 17:18:34 +0900

    ss

---
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/js/core.js
@@ -0,0 +1,8 @@
+
+/////// 로딩구역 //////////////////////
+window.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelector(".flove").innerHTML=svgData.loveout;
+    document.querySelector(".elove").innerHTML=svgData.lovein;
+
+}); /////////////// 로딩구역 ////////////////////
