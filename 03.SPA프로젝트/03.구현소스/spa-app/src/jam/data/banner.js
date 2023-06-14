1d9c61bf28a33c25cd32ff7af4e8c530aa23334f
kum1416 <kum1416@naver.com>
2023-06-15 00:10:17 +0900

    ss

---
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/jam/data/banner.js
@@ -0,0 +1,98 @@
+// 배너 컴포넌트 사용 데이터 - data/banner.js
+
+
+const ban_data = [
+    {
+        isrc:"./images/banner/jam1.jpg",
+        hsrc:"./images/banner/jamhover1.jpg",
+        tit:"Hazelnut Butter Crunchy with Cocoa & Date",
+        price:"€7,90",
+        item:"NUT BUTTERS",
+        color:"#abafd6",
+    },
+    {
+        isrc:"./images/banner/jam2.jpg",
+        hsrc:"./images/banner/jamhover2.jpg",
+        tit:"Peanut Butter",
+        price:"€5,20",
+        item:"NUT BUTTERS",
+        color:"#72a080",
+    },
+    {
+        isrc:"./images/banner/jam3.jpg",
+        hsrc:"./images/banner/jamhover3.jpg",
+        tit:"Tahini with Cocoa & Honey",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#a95d50",
+    },
+    {
+        isrc:"./images/banner/jam4.jpg",
+        hsrc:"./images/banner/jamhover4.jpg",
+        tit:"Hazelnut Butter",
+        price:"€6,90",
+        item:"NUT BUTTERSI",
+        color:"#abafd6",
+    },
+    {
+        isrc:"./images/banner/jam5.jpg",
+        hsrc:"./images/banner/jamhover5.jpg",
+        tit:"Tahini",
+        price:"€4,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#fecf6b",
+    },
+    {
+        isrc:"./images/banner/jam6.jpg",
+        hsrc:"./images/banner/jamhover6.jpg",
+        tit:"Cashew Butter",
+        price:"€6,90",
+        item:"NUT BUTTERS",
+        color:"#afe2f8",
+    },
+    {
+        isrc:"./images/banner/jam7.jpg",
+        hsrc:"./images/banner/jamhover7.jpg",
+        tit:"Tahini with Honey",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#fff7b0",
+    },
+    {
+        isrc:"./images/banner/jam8.jpg",
+        hsrc:"./images/banner/jamhover8.jpg",
+        tit:"High Fiber Tahini",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#a6d07d",
+    },
+    {
+        isrc:"./images/banner/jam9.jpg",
+        hsrc:"./images/banner/jamhover9.jpg",
+        tit:"Almond Butter",
+        price:"€7,20",
+        item:"NUT BUTTERS",
+        color:"#f6b8d3",
+    },
+    {
+        isrc:"./images/banner/jam10.jpg",
+        hsrc:"./images/banner/jamhover10.jpg",
+        tit:"High Protein Tahini",
+        price:"€6,40",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#f84f5a",
+    },
+    {
+        isrc:"./images/banner/jam11.jpg",
+        hsrc:"./images/banner/jamhover11.jpg",
+        tit:"Tahini with Chia Seeds",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#ad6aa9",
+    },
+];
+
+
+
+// 데이터 내보내기
+export default ban_data;
