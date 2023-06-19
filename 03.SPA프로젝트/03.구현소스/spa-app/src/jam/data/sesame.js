4260e0be6fcb5ecb8485a492f1430493039f38e6
kum1416 <kum1416@naver.com>
2023-06-19 18:18:32 +0900

    ss

---
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/jam/data/sesame.js
@@ -0,0 +1,106 @@
+// 배너 컴포넌트 사용 데이터 - data/banner.js
+
+
+const sesame_data = [
+    {
+        isrc:"./images/sesame/sesame1.jpg",
+        hsrc:"./images/sesame/sesamehover1.jpg",
+        tit:"Tahini",
+        price:"€4,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#fecf6b",
+    },
+    {
+        isrc:"./images/sesame/sesame2.jpg",
+        hsrc:"./images/sesame/sesamehover2.jpg",
+        tit:"Tahini with Cocoa & Honey",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#a95d50",
+    },
+    {
+        isrc:"./images/sesame/sesame3.jpg",
+        hsrc:"./images/sesame/sesamehover3.jpg",
+        tit:"High Fiber Tahiniy",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#a6d07d",
+    },
+    {
+        isrc:"./images/sesame/sesame4.jpg",
+        hsrc:"./images/sesame/sesamehover4.jpg",
+        tit:"Tahini with Honey",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#fff7b0",
+    },
+    {
+        isrc:"./images/sesame/sesame5.jpg",
+        hsrc:"./images/sesame/sesamehover5.jpg",
+        tit:"Tahini Almond Butter Peanut Butter Combo",
+        price:"€6,40",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#f6b8d3",
+    },
+    {
+        isrc:"./images/sesame/sesame6.jpg",
+        hsrc:"./images/sesame/sesamehover6.jpg",
+        tit:"High Protein Tahini",
+        price:"€6,40",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#f84f5a",
+    },
+    {
+        isrc:"./images/sesame/sesame7.jpg",
+        hsrc:"./images/sesame/sesamehover7.jpg",
+        tit:"Tahini with Coconut",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#aed7ad",
+    },
+    {
+        isrc:"./images/sesame/sesame8.jpg",
+        hsrc:"./images/sesame/sesamehover8.jpg",
+        tit:"Tahini with Linseeds",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#4fb07d",
+    },
+    {
+        isrc:"./images/sesame/sesame9.jpg",
+        hsrc:"./images/sesame/sesamehover9.jpg",
+        tit:"Tahini with Carob Syrup",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#a790c2",
+    },
+    {
+        isrc:"./images/sesame/sesame10.jpg",
+        hsrc:"./images/sesame/sesamehover10.jpg",
+        tit:"Whole Grain Tahini",
+        price:"€4,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#d4b18a",
+    },
+    {
+        isrc:"./images/sesame/sesame11.jpg",
+        hsrc:"./images/sesame/sesamehover11.jpg",
+        tit:"Tahini with Chia Seeds",
+        price:"€5,90",
+        item:"SESAME ΒUTTER / TAHINI",
+        color:"#ad6aa9",
+    },
+    {
+        isrc:"./images/sesame/sesame12.jpg",
+        hsrc:"./images/sesame/sesamehover12.jpg",
+        tit:"Halva with Honey",
+        price:"€8,90",
+        item:"HALVA",
+        color:"#c88a6b",
+    },
+];
+
+
+
+// 데이터 내보내기
+export default sesame_data;
