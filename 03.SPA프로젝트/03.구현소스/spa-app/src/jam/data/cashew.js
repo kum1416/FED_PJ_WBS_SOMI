fd08c31faf851d888e719a71f5232ff974f275a1
kum1416 <kum1416@naver.com>
2023-06-20 13:18:41 +0900

    ss

--- 03.SPA프로젝트/03.구현소스/spa-app/src/jam/data/cashew.js
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/jam/data/cashew.js
@@ -1,14 +1,38 @@
 // 배너 컴포넌트 사용 데이터 - data/banner.js
 
 
-const almond_data = [
+const cashew_data = [
     {
-        isrc:"./images/almond/almond1.jpg",
-        hsrc:"./images/almond/almondhover1.jpg",
-        tit:"Pistachio Butter",
-        price:"€13,50",
+        isrc:"./images/cashew/cashew1.jpg",
+        hsrc:"./images/cashew/cashewhover1.jpg",
+        tit:"Cashew Butter",
+        price:"€6,90",
         item:"NUT BUTTERS",
-        color:"#b9d478",
+        color:"#afe2f8",
+    },
+    {
+        isrc:"./images/cashew/cashew2.jpg",
+        hsrc:"./images/cashew/cashewhover2.jpg",
+        tit:"Halva with Cashew Nuts & Honey",
+        price:"€10,70",
+        item:"HALVA",
+        color:"#c88a6b",
+    },
+    {
+        isrc:"./images/cashew/cashew3.jpg",
+        hsrc:"./images/cashew/cashewhover3.jpg",
+        tit:"Roasted Cashew",
+        price:"€5,50",
+        item:"DRY NUTS",
+        color:"#a5493d",
+    },
+    {
+        isrc:"./images/cashew/cashew4.jpg",
+        hsrc:"./images/cashew/cashewhover4.jpg",
+        tit:"Raw Cashew",
+        price:"€5,25",
+        item:"DRY NUTS",
+        color:"#fceeea",
     },
 
 ];
@@ -16,4 +40,4 @@ const almond_data = [
 
 
 // 데이터 내보내기
-export default almond_data;
+export default cashew_data;
--- 03.SPA프로젝트/03.구현소스/spa-app/src/jam/data/hazelnut.js
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/jam/data/hazelnut.js
@@ -1,14 +1,46 @@
 // 배너 컴포넌트 사용 데이터 - data/banner.js
 
 
-const almond_data = [
+const hazelnut_data = [
     {
-        isrc:"./images/almond/almond1.jpg",
-        hsrc:"./images/almond/almondhover1.jpg",
-        tit:"Pistachio Butter",
-        price:"€13,50",
+        isrc:"./images/hazelnut/hazelnut1.jpg",
+        hsrc:"./images/hazelnut/hazelnuthover1.jpg",
+        tit:"Hazelnut Butter",
+        price:"€6,90",
         item:"NUT BUTTERS",
-        color:"#b9d478",
+        color:"#abafd6",
+    },
+    {
+        isrc:"./images/hazelnut/hazelnut2.jpg",
+        hsrc:"./images/hazelnut/hazelnuthover2.jpg",
+        tit:"Hazelnut Butter Crunchy",
+        price:"€6,90",
+        item:"NUT BUTTERS",
+        color:"#abafd6",
+    },
+    {
+        isrc:"./images/hazelnut/hazelnut3.jpg",
+        hsrc:"./images/hazelnut/hazelnuthover3.jpg",
+        tit:"Hazelnut Butter with Cocoa",
+        price:"€6,90",
+        item:"NUT BUTTERS",
+        color:"#abafd6",
+    },
+    {
+        isrc:"./images/hazelnut/hazelnut4.jpg",
+        hsrc:"./images/hazelnut/hazelnuthover4.jpg",
+        tit:"Hazelnut Butter Halva with Honey",
+        price:"€9,90",
+        item:"HALVA",
+        color:"#abafd6",
+    },
+    {
+        isrc:"./images/hazelnut/hazelnut5.jpg",
+        hsrc:"./images/hazelnut/hazelnuthover5.jpg",
+        tit:"Roasted Hazelnut",
+        price:"€5,50",
+        item:"DRY NUTS",
+        color:"#a5493d",
     },
 
 ];
@@ -16,4 +48,4 @@ const almond_data = [
 
 
 // 데이터 내보내기
-export default almond_data;
+export default hazelnut_data;
